41aeea62feb44158758fd2ac458052c59dc1b7b8
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2023-11-28 21:09:40 +0530

    Stacks

--- Stacks.c
+++ Stacks.c
@@ -11,82 +11,49 @@ Display : Display current elements in Stack
 */
 #include<stdio.h>
 
-void enstack();
-void destack();
-void display();
-int stack[50];
 int top=-1;
 int size=0;
-int main()
-{
-    int choice;
-    
-    
-    printf("\n------- Stacks ------\n");
-    printf("\nEnter the size of stack(Max 50):\t");
-    scanf("%d",&size);
-    while(1){
-        printf("\n1.Enstack\n2.Destack\n3.Display\n4.Exit\n");
-        printf("\nChoose your option:\t");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1: enstack();
-                    break;
-            case 2: destack();
-                    break;
-            case 3: display();
-                    break;
-            case 4: return 0;
-            default: printf(">>> Error: Enter Valid Option <<<");
-        }
-    }
-}
 
-void enstack()
+void enstack(int *stack)
 {
     int elem;
 
-    if(top==size-1)
-    {
-        printf("\n>>> Error: Stack OverFlow <<<\n");
+    if(top==size-1) {
+        printf("\n>>> Error: Stack OverFlow! <<<\n");
         return;
     }
-    else
-    {
+    else {
         printf("\nEnter the element to be inserted:\t");
         scanf("%d",&elem);
         stack[++top]=elem;
-        printf("%d has been pushed\n",stack[top]);
+        printf(">> %d has been inserted\n",stack[top]);
     }
     return;
 }
-void destack()
+
+void destack(int *stack)
 {
     int elem;
     
-    if(top==-1)
-    {
-        printf("\n >>> Error: Stack UnderFlow <<<\n");
+    if(top==-1) {
+        printf("\n >>> Error: Stack UnderFlow! <<<\n");
         return;
     }
-    else
-    {
-        printf("\n%d has been removed\n",stack[top--]);
+    else {
+        printf(">> %d has been removed\n",stack[top--]);
     }
     return;
 }
-void display()
+
+void display(int *stack)
 {
     int i=0;
     
-    if(top==-1)
-    {
-        printf("\n >>> Error: Stack UnderFlow <<<\n");
+    if(top==-1) {
+        printf("\n >>> Error: Stack UnderFlow! <<<\n");
         return;
     }
-    else
-    {
+    else {
         printf("\n|");
         for(i=0;i<=top;i++)
         {
@@ -95,4 +62,32 @@ void display()
         printf("\n");
     }
     return;
+}
+
+int main()
+{
+    int choice;
+    
+    printf("\n------- Stacks ------\n");
+    printf("\nEnter the size of stack:\t");
+    scanf("%d",&size);
+    int stack[size];
+
+    while(1){
+        printf("\n---------------------\n");
+        printf("1.Enstack\n2.Destack\n3.Display\n4.Exit\n");
+        printf(">> Choose your option:\t");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1: enstack(stack);
+                    break;
+            case 2: destack(stack);
+                    break;
+            case 3: display(stack);
+                    break;
+            case 4: return 0;
+            default: printf(">>> Error: Enter Valid Option <<<");
+        }
+    }
 }
