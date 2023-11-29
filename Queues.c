34a4617268017f6b620fd8e9b33fa148a25e4823
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2023-11-29 21:27:12 +0530

    Queues

--- Queues.c
+++ Queues.c
@@ -9,91 +9,86 @@ Dequeue : Popping Elements from Queue
 Display : Display current elements in Queue
 
 */
-#include<stdio.h>
+#include <stdio.h>
 
-void enqueue();
-void dequeue();
-void display();
-int queue[50];
-int rear=-1;
-int front=0;
-int size=0;
+int rear = -1;
+int front = 0;
+int size = 0;
+
+void enqueue(int *queue)
+{
+    int elem;
+
+    if ((rear - front) == size-1) {
+        printf("\n>>> Error: Queue OverFlow <<<\n");
+        return;
+    }
+    else {
+        printf("\nEnter the element to be inserted:\t");
+        scanf("%d", &elem);
+        printf("front:%d, rear:%d\n", front, rear);
+        queue[++rear] = elem;
+        printf("%d has been pushed.\n", queue[rear]);
+    }
+    return;
+}
+void dequeue(int *queue)
+{
+    int elem;
+    
+    if (rear < front) {
+        printf("\n>>> Error: Queue UnderFlow <<<\n");
+        return;
+    }
+    else {
+        printf("front:%d, rear:%d\n", front, rear);
+        printf("\n%d has been removed\n", queue[front++]);
+    }
+    return;
+}
+void display(int *queue)
+{
+    int i=0;
+    
+    if (rear < front) {
+        printf("front:%d, rear:%d", front, rear);
+        printf("\n>>> Error: Queue Empty <<<\n");
+        return;
+    }
+    else {
+        printf("front:%d, rear:%d\n", front, rear);
+        printf("\n|");
+        for (i = front; i <= rear; i++) {
+            printf(" %d |", queue[i]);
+        }
+        printf("\n");
+    }
+    return;
+}
+
 int main()
 {
     int choice;
     
-    
     printf("\n------- Queue ------\n");
-    printf("\nEnter the size of queue(Max 50):\t");
-    scanf("%d",&size);
-    while(1){
+    printf("\nEnter the size of queue:\t");
+    scanf("%d", &size);
+    int queue[size];
+
+    while(1) {
         printf("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
         printf("\nChoose your option:\t");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
         switch(choice)
         {
-            case 1: enqueue();
+            case 1: enqueue(queue);
                     break;
-            case 2: dequeue();
+            case 2: dequeue(queue);
                     break;
-            case 3: display();
+            case 3: display(queue);
                     break;
             case 4: return 0;
             default: printf("\n>> Error: Enter Valid Option <<\n");
         }
     }
-}
-
-void enqueue()
-{
-    int elem;
-
-    if((rear-front)==size-1)
-    {
-        printf("\n>>> Error: Queue OverFlow <<<\n");
-        return;
-    }
-    else
-    {
-        printf("\nEnter the element to be inserted:\t");
-        scanf("%d",&elem);
-        queue[++rear]=elem;
-        printf("%d has been pushed\n",queue[rear]);
-    }
-    return;
-}
-void dequeue()
-{
-    int elem;
-    
-    if(rear<front)
-    {
-        printf("\n>>> Error: Queue UnderFlow <<<\n");
-        return;
-    }
-    else
-    {
-        printf("\n%d has been removed\n",queue[front++]);
-    }
-    return;
-}
-void display()
-{
-    int i=0;
-    
-    if(front==-1)
-    {
-        printf("\n>>> Error: Queue UnderFlow <<<\n");
-        return;
-    }
-    else
-    {
-        printf("\n|");
-        for(i=front;i<=rear;i++)
-        {
-            printf(" %d |",queue[i]);
-        }
-        printf("\n");
-    }
-    return;
 }
