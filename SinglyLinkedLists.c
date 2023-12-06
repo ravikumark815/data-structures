90088c75cde3409ce94a79fcc824762d7bf55f5e
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2023-12-06 17:36:01 +0530

    Singly Linked Lists

--- SinglyLinkedLists.c
+++ SinglyLinkedLists.c
@@ -34,33 +34,33 @@ void enlink()
     scanf("%d",&item);
     temp = (NODE*)malloc(sizeof(NODE));
     temp->data = item;
-    if(first==NULL){
+    if (first == NULL){
         first = temp;
         temp->link = NULL;
         printf("%d has been inserted at the beginning\n",cur->data);
         return;
     }
-    else{
+    else {
         printf("\n1.Beginning\n2.Ending\n3.InBetween");
         printf("\nPosition to be inserted in:\t");
         scanf("%d",&choice);
-        if(choice==1){
+        if (choice == 1){
             first = temp;
             temp->link = NULL;
             printf("%d has been inserted at the beginning\n",cur->data);
         }
-        else if(choice==2){
+        else if (choice == 2){
             while(cur->link!=NULL)
                 cur = cur->link;
             cur->link = temp;
             temp->link = NULL;
             printf("%d has been inserted at the ending\n",temp->data);
         }
-        else if(choice==3){
+        else if (choice == 3){
             NODE prev;
             printf("\nEnter the value of element before which the element should be inserted\t:");
             scanf("%d",&pos);
-            while(cur->data!=pos){
+            while (cur->data != pos) {
                 prev = cur;
                 cur = cur->link;
             }
@@ -80,20 +80,20 @@ void delink()
 
     cur = first;
     
-    if(cur->link = NULL){
+    if (cur->link = NULL) {
         printf("\n>>> Error: Linked List UnderFlow <<<\n");
         return;
     }
-    else{
+    else {
         printf("\n1.Beginning\n2.Ending\n3.InBetween");
         printf("\nPosition to be deleted from:\t");
         scanf("%d",&choice);
-        if(choice==1){
+        if (choice == 1) {
             first = cur->link;
             printf("%d has been deleted\n",cur->data);
             free(cur);
         }
-        else if(choice==2){
+        else if (choice == 2) {
             while(cur->link!=NULL)
                 cur = cur->link;
             prev = cur;
@@ -102,15 +102,15 @@ void delink()
             printf("%d has been deleted\n",cur->data);
             free(cur);
         }
-        else if(choice==3){
+        else if (choice == 3) {
             printf("\nEnter the element before which the element should be deleted\t:");
             scanf("%d",&pos);
-            while(cur->data!=pos){
+            while (cur->data != pos) {
                 prev = cur;
                 cur = cur->link;
             }
             prev->link = cur->link;
-            printf("%d has been deleted",cur->data);
+            printf("%d has been deleted", cur->data);
             free(cur);
         }
     }
@@ -121,15 +121,15 @@ void display()
 {
     NODE cur = first;
 
-    if(cur->link == NULL){
+    if (cur->link == NULL) {
         printf("\n>>> Error: Linked List Underflow <<<\n");
     }
-    else{
-        while(cur->link!=NULL){
-            printf("%d -> ",cur->data);
+    else {
+        while (cur->link != NULL){
+            printf("%d -> ", cur->data);
             cur = cur->link;
         }
-        printf("%d\n",(cur->link)->data);
+        printf("%d\n", (cur->link)->data);
     }
 }
 
@@ -139,10 +139,10 @@ int main()
 
     printf("\n------- Singly Linked List -------\n");
 
-    while(1){
+    while(1) {
         printf("\n1.Enlink\n2.Delink\n3.Display\n4.Exit\n");
         printf("\nChoose your Option:\t");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
         switch(choice)
         {
             case 1: enlink();
