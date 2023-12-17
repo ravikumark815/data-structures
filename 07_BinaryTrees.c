ecf54a3415d086733adf2517419a631cbc853b2b
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2023-12-17 12:57:18 +0530

    Binary Trees

--- 07_BinaryTrees.c
+++ 07_BinaryTrees.c
@@ -20,56 +20,34 @@ struct node
 };
 
 typedef struct node* NODE; // Declare a type named NODE for easy use
-NODE root;
-
-void init_list()
-{
-    root = NULL;
-}
-
-/*
-Function    : create_node
-Purpose     : To create a new NODE with given data and childs as NULL
-*/
-NODE create_node()
-{
-    int data;
-    NODE temp;
-    temp = (NODE)malloc(sizeof(struct node));
-    
-    printf("Enter the element to Insert:\t");
-    scanf("%d", &data);
-    if (temp == NULL) {
-        printf(">>> Error: Insufficient Memory\n");
-        exit (0);
-    }
-    temp->data = data;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
 
 /*
 Function    : insert
 Purpose     : To insert node into the binary tree
 */
-void insert()
+NODE insert(NODE root, int element)
 {
-    NODE temp;
-    
-    temp = create_node();
-
     if (root == NULL) {
-        root = temp;
-        return;
+        root = (NODE)malloc(sizeof(struct node));;
+        if (root == NULL) {
+            printf(">>> Error: Insufficient Memory\n");
+            exit (0);
+        }
+        root->data = element;
+        root->left = NULL;
+        root->right = NULL;
+        printf("%d has been inserted\n", root->data);
     }
-    temp->right = root;
-    root = temp;
-    root->right->left = temp;
-    
-    printf("%d has been inserted at the beginning\n", root->data);    
-    return;
+
+    if (element < root->data) {
+        printf("L\t");
+        root->left = insert(root->left, element);
+    }
+    else if (element > root->data){
+        printf("R\t");
+        root->right = insert(root->right, element);
+    }
+    return root;
 }
 
 /*
@@ -77,7 +55,7 @@ Function    : delete
 Purpose     : To delete a node from the binary tree
 */
 
-void delete()
+void delete(NODE root)
 {
     NODE cur = root;
     
@@ -97,34 +75,28 @@ void delete()
 Function    : Display
 Purpose     : To display the current elements in binary tree
 */
-void display()
+void display(NODE root, int level)
 {
     if (root == NULL) {
-        printf("\n>>> Error: Binary Tree Empty <<<\n");
         return;
     }
-    
-    NODE cur = root;
-    int count = 0;
-    
-    printf("\n");
-    while (cur->right != NULL){
-        printf("|%d|->", cur->data);
-        cur = cur->right;
-        count++;
+
+    for (int i = 0; i <= level; i++) {
+        printf("  ");
     }
-    printf("|%d|\n", cur->data);
-    printf("\nNo. of nodes: %d\n", count+1);
-    
-    return;
+    printf("%d\n", root->data);
+
+    display(root->left, level + 1);
+    display(root->right, level + 1);
 }
 
 int main()
 {
-    int choice;
+    int choice, element;
     
     printf("\n------- Binary Trees ------\n");
 
+    NODE root = NULL;
     while(1){
         printf("\n---------------------\n");
         printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
@@ -132,11 +104,13 @@ int main()
         scanf("%d", &choice);
         switch(choice)
         {
-            case 1: insert();
+            case 1: printf("Enter the element to be inserted: ");
+                    scanf("%d", &element);
+                    root = insert(root, element);
                     break;
-            case 2: delete();
+            case 2: delete(root);
                     break;
-            case 3: display();
+            case 3: display(root, 0);
                     break;
             case 4: return 0;
             default: printf(">>> Error: Enter Valid Option <<<");
