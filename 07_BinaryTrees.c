d44aa889e5118c52b46fd63b69231262aee0cd6f
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2023-12-22 09:44:54 +0530

    Binary Trees

--- 07_BinaryTrees.c
+++ 07_BinaryTrees.c
@@ -4,9 +4,11 @@ Author: https://github.com/ravikumark815
 
 ------- Binary Trees -------
 
-Insert : Inserting Elements to Binary Tree
-Delete : Deleting Elements from Binary Tree
+Insert  : Inserting Elements to Binary Tree
+Delete  : Deleting Elements from Binary Tree
 Display : Display current elements in Binary Tree
+findMax : Returns highest element of the tree
+findMin : Returns lowest element of the tree
 
 */
 #include <stdio.h>
@@ -20,6 +22,34 @@ struct node
 };
 
 typedef struct node* NODE; // Declare a type named NODE for easy use
+
+/*
+Function    : findMax
+Purpose     : To find the highest element in a tree
+*/
+NODE findMax(NODE root)
+{
+    if(root == NULL)
+    	return NULL;
+    else if(root->right == NULL)
+    	return root;
+    else
+    	return findMax(root->right);
+}
+
+/*
+Function    : findMax
+Purpose     : To find the lowest element in a tree
+*/
+NODE findMin(NODE root)
+{
+    if(root == NULL)
+    	return NULL;
+    else if(root->left == NULL)
+    	return root;
+    else
+    	return findMin(root->left);
+}
 
 /*
 Function    : insert
@@ -65,14 +95,20 @@ NODE delete(NODE root, int element)
     else if (element > root->data) {
         root->right = delete(root->right, element);
     }
-    else if (root->data == element) { 
-        if (root->left && root->right){
-
-        }
-        else if (root->left == NULL){
-            root = root->right;
-        }
+    else if (root->left && root->right) { 
+        NODE temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, root->data);
     }
+    else {
+        NODE temp = root;
+        if(root->left == NULL)
+            root = root->right;
+        else if(root->right == NULL)
+            root = root->left;
+        free(temp);
+    }
+    return root;
 }
 
 /*
@@ -85,13 +121,13 @@ void display(NODE root, int level)
         return;
     }
 
-    for (int i = 0; i <= level; i++) {
+    display(root->right, level + 1);
+    for (int i = 0; i < level; ++i) {
         printf("  ");
     }
     printf("%d\n", root->data);
 
     display(root->left, level + 1);
-    display(root->right, level + 1);
 }
 
 int main()
@@ -103,7 +139,7 @@ int main()
     NODE root = NULL;
     while(1){
         printf("\n---------------------\n");
-        printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+        printf("1.Insert\n2.Delete\n3.Display\n4.Highest Element\n5.Lowest Element\n6.Exit\n");
         printf(">> Choose your option:\t");
         scanf("%d", &choice);
         switch(choice)
@@ -118,7 +154,11 @@ int main()
                     break;
             case 3: display(root, 0);
                     break;
-            case 4: return 0;
+            case 4: printf("Highest Element is %d:", findMax(root)->data);
+                    break;
+            case 5: printf("Lowest Element is %d:", findMax(root)->data);
+                    break;
+            case 6: return 0;
             default: printf(">>> Error: Enter Valid Option <<<");
         }
     }
